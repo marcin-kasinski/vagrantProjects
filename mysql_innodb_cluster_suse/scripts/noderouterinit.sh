b725bc86d4e1d1a9d29e96fdce220118f49c06fa
marcin <marcin.kasinski@gmail.com>
2018-03-05 18:33:20 +0100

    TST

--- mysql_innodb_cluster_suse/scripts/noderouterinit.sh
+++ mysql_innodb_cluster_suse/scripts/noderouterinit.sh
@@ -1,6 +1,26 @@
 IP=$1
 
-MYSQL_PASS=secret
 echo $IP
 
 echo I am provisioning ...
+
+#sudo sed -i -r '/openstackmaster/ s/^(.*)$/#\1/g' /etc/hosts
+
+#sudo sh -c "echo '192.168.33.10 openstackmaster' >> /etc/hosts"
+
+sudo sh -c "echo '192.168.44.10 mysqlnode1' >> /etc/hosts"
+sudo sh -c "echo '192.168.44.11 mysqlnode2' >> /etc/hosts"
+sudo sh -c "echo '192.168.44.12 mysqlnode3' >> /etc/hosts"
+sudo sh -c "echo '192.168.44.13 mysqlrouter' >> /etc/hosts"
+
+wget https://dev.mysql.com/get/mysql57-community-release-sles12-11.noarch.rpm -O /tmp/mysql57-community-release-sles12-11.noarch.rpm
+
+sudo rpm -ivh /tmp/mysql57-community-release-sles12-11.noarch.rpm
+
+sudo zypper --non-interactive --no-gpg-checks --gpg-auto-import-keys ref 
+
+sudo zypper --non-interactive --no-gpg-checks install mysql-router
+
+
+#sudo mysqlrouter --bootstrap root@mysqlnode1:3306 --user=mysqlrouter
+#sudo mysqlrouter &
--- mysql_innodb_cluster_suse/shell-js/cluster.js
+++ mysql_innodb_cluster_suse/shell-js/cluster.js
@@ -1,14 +1,16 @@
 
-#dba.checkInstanceConfiguration('root@localhost:3306');
+dba.checkInstanceConfiguration('root@localhost:3306', {password: 'Secretqaz@wsx123'});
 
-#dba.configureLocalInstance('root@localhost:3306', {password: 'Secretqaz@wsx123'});
+dba.configureLocalInstance('root@localhost:3306', {password: 'Secretqaz@wsx123'});
 
-#shell.connect('root@192.168.44.10:3306', 'Secretqaz@wsx123');
-#var cluster = dba.createCluster('myCluster');
-#cluster.status();
-#cluster.addInstance('root@192.168.44.11:3306', {password: 'Secretqaz@wsx123'});
-#cluster.addInstance('root@192.168.44.12:3306', {password: 'Secretqaz@wsx123'});
-#cluster.status();
+shell.connect('root@192.168.44.10:3306', 'Secretqaz@wsx123');
+var cluster = dba.createCluster('myCluster');
+cluster.status();
+cluster.addInstance('root@192.168.44.11:3306', {password: 'Secretqaz@wsx123'});
+cluster.addInstance('root@192.168.44.12:3306', {password: 'Secretqaz@wsx123'});
+cluster.status();
 
 
 var cluster = dba.getCluster('myCluster');
+#dba.rebootClusterFromCompleteOutage('myCluster')
+cluster.forceQuorumUsingPartitionOf("localhost:3306")
