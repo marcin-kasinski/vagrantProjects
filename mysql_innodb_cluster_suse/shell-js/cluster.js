de58b4e5953b4647ec12915318d5332c6ffe9e6c
marcin <marcin.kasinski@gmail.com>
2018-03-06 18:45:02 +0100

    TST

--- mysql_innodb_cluster_suse/shell-js/cluster.js
+++ mysql_innodb_cluster_suse/shell-js/cluster.js
@@ -1,7 +1,13 @@
 
-dba.checkInstanceConfiguration('root@localhost:3306', {password: 'Secretqaz@wsx123'});
 
 dba.configureLocalInstance('root@localhost:3306', {password: 'Secretqaz@wsx123'});
+
+dba.checkInstanceConfiguration('root@localhost:3306', {password: 'Secretqaz@wsx123'});
+
+
+dba.checkInstanceState('root@localhost:3306', {password: 'Secretqaz@wsx123'});
+
+
 
 shell.connect('root@192.168.44.10:3306', 'Secretqaz@wsx123');
 var cluster = dba.createCluster('myCluster');
@@ -12,5 +18,13 @@ cluster.status();
 
 
 var cluster = dba.getCluster('myCluster');
+cluster.status();
 #dba.rebootClusterFromCompleteOutage('myCluster')
-cluster.forceQuorumUsingPartitionOf("localhost:3306")
+#cluster.forceQuorumUsingPartitionOf("localhost:3306")
+
+
+
+#rejoining
+
+cluster = dba.getCluster()
+cluster.rejoinInstance('192.168.44.12:3306', {password: 'Secretqaz@wsx123'})
