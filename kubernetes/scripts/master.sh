674b10f9ba003291e1a9270a51e346241a78c1f7
marcin <marcin.kasinski@gmail.com>
2018-04-18 20:31:54 +0200

    .

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -83,6 +83,7 @@ echo Dashboard IP $DASHBOARDPODIP
 #      curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/kafkaspotify.yml | kubectl apply -f -     
       curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/kafka1.0.yml | kubectl apply -f -     
 #      curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/kafka_ds.yml?$(date +%s)"  | kubectl apply -f -     
+#      curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/zookeeper_ss.yml?$(date +%s)"  | kubectl apply -f -     
 
       curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/mysql_dp_and_service.yaml | kubectl apply -f -     
       curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/phpmyadmin_dp_and_service.yaml | kubectl apply -f -     
