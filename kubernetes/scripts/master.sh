39ecb3d3105e59f258f5c57314aac61628f3a0b6
marcin <marcin.kasinski@gmail.com>
2018-08-23 15:32:35 +0200

    add patch

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -1,3 +1,95 @@
+forwardingressport()
+{
+
+while ! kubectl get po -n ingress-nginx -o wide | grep nginx | grep k8snode1 | grep Running ; do   echo "waiting for ingress-nginx IP..." ; sleep 20 ; done
+
+INGRESSPODNAME=`kubectl get po -n ingress-nginx -o wide | grep nginx | grep k8snode1 | grep Running `
+
+echo  $INGRESSPODNAME
+
+INGRESSPODNAME=`echo $INGRESSPODNAME | cut -d " " -f 1`
+echo  $INGRESSPODNAME
+
+INGRESSPODPORT=`kubectl get svc -n ingress-nginx ingress-nginx|grep ingress-nginx `
+INGRESSPODPORT=`echo $INGRESSPODPORT | cut -d ":" -f 2`
+INGRESSPODPORT=`echo $INGRESSPODPORT | cut -d "/" -f 1`
+echo  $INGRESSPODPORT
+
+
+}
+
+setupkafka()
+{
+
+          #------------------------------- kafka init ------------------------------- 
+     
+      sudo apt install -y openjdk-8-jdk
+      while ! kubectl get po -o wide | grep kafka-0 | grep Running ; do   echo "waiting for kafka IP..." ; sleep 20 ; done
+
+
+      KAFKAPODIP=`kubectl get po -o wide | grep kafka-0 | grep Running `
+      echo $KAFKAPODIP
+      KAFKAPODIP=`echo $KAFKAPODIP  | cut -d " " -f 6`
+      echo $KAFKAPODIP
+
+      while ! nc -z $KAFKAPODIP 9092; do   echo "waiting kafka to launch ..." ; sleep 20 ; done
+
+
+		cd /tmp
+		curl http://ftp.ps.pl/pub/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz | tar xvz
+		/tmp/kafka_2.11-1.0.0/bin/kafka-topics.sh --list --zookeeper $KAFKAPODIP:2181
+
+
+          #------------------------------- kafka init ------------------------------- 
+
+}
+
+setupMYSQL()
+{
+
+
+      #------------------------------- mysql init ------------------------------- 
+            
+      sudo apt install -y netcat
+      sudo apt install -y mysql-client 
+            
+
+while ! kubectl get po -o wide | grep mysql | grep Running ; do   echo "waiting for mysql IP..." ; sleep 20 ; done
+
+while ! nc -w 20 -z $MYSQLPODIP 3306; 
+do
+  while ! kubectl get po -o wide | grep mysql | grep Running ; do   echo "waiting for mysql IP..." ; sleep 20 ; done
+
+  MYSQLPODIP=`kubectl get po -o wide | grep mysql | grep Running `
+  MYSQLPODIP=`echo $MYSQLPODIP  | cut -d " " -f 6`
+  echo $MYSQLPODIP
+  echo "waiting mysql ( $MYSQLPODIP ) to launch ..." ; sleep 20 ; 
+done
+
+
+	echo "Found MYSQL : $MYSQLPODIP"
+
+      mysqlshow -h $MYSQLPODIP --user=root --password=secret mysql | grep -v Wildcard | grep -o test
+
+         if [ $? -gt 0 ] ; then
+          echo "nie ma bazy"
+          
+          
+          
+          #echo $SQL >microserviceinit.sql
+          #mysql -h $MYSQLPODIP  -uroot -psecret  --port 3306  mysql -e "$SQL"
+          mysql -h $MYSQLPODIP  -uroot -psecret  --port 3306  mysql < /vagrant/sql/microserviceinit.sql
+        else
+          echo "jest baza"
+    	fi
+    
+    
+    
+          #------------------------------- mysql init ------------------------------- 
+
+
+}
+
 
 showCustomService()
 {
@@ -56,11 +148,14 @@ showDashboardIP(){
 
 while ! kubectl get po -n kube-system -o wide | grep kubernetes-dashboard | grep Running ; do   echo "waiting for dashboard IP..." ; sleep 20 ; done
 
-DASHBOARDPODIP=`kubectl get po -n kube-system -o wide | grep kubernetes-dashboard | grep Running`
-DASHBOARDPODIP=`echo $DASHBOARDPODIP  | cut -d " " -f 6`
+	DASHBOARDPODIP=`kubectl get po -n kube-system -o wide | grep kubernetes-dashboard | grep Running`
+	
+	DASHBOARDPODNAME=`echo $DASHBOARDPODIP  | cut -d " " -f 1`
+	DASHBOARDPODIP=`echo $DASHBOARDPODIP  | cut -d " " -f 6`
+	echo Dashboard Name: $DASHBOARDPODNAME
+	echo Dashboard IP $DASHBOARDPODIP
 
-echo Dashboard IP $DASHBOARDPODIP
-
+#sudo kubectl port-forward  -n kube-system $DASHBOARDPODNAME 8443:8443 &
 
       echo "DashboardToken ..."
       
@@ -129,6 +224,11 @@ echo Dashboard IP $DASHBOARDPODIP
       sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
       sudo chown vagrant:vagrant /home/vagrant/.kube/config
 
+
+      #copy to NFS
+      sudo cp -i /etc/kubernetes/admin.conf /var/nfs/kubernetes_share/
+
+
       echo ">>>>>>>>>>>>>>>>>>>>>>>>>>CREATING CONF"
 
       kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
@@ -165,61 +265,8 @@ echo Dashboard IP $DASHBOARDPODIP
       curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/SpringBootZipkin_dp_and_service.yaml?$(date +%s)"  | kubectl apply -f -
 
 
-      #------------------------------- mysql init ------------------------------- 
-            
-      sudo apt install -y netcat
-      sudo apt install -y mysql-client 
-            
-      while ! kubectl get po -o wide | grep mysql | grep Running ; do   echo "waiting for mysql IP..." ; sleep 20 ; done
-
-      MYSQLPODIP=`kubectl get po -o wide | grep mysql | grep Running `
-      echo $MYSQLPODIP
-      MYSQLPODIP=`echo $MYSQLPODIP  | cut -d " " -f 6`
-      echo $MYSQLPODIP
-      
-      while ! nc -w 20 -z $MYSQLPODIP 3306; do   echo "waiting mysql to launch ..." ; sleep 20 ; done
-
-
-	echo "Found MYSQL"
-
-      mysqlshow -h $MYSQLPODIP --user=root --password=secret mysql | grep -v Wildcard | grep -o test
-
-         if [ $? -gt 0 ] ; then
-          echo "nie ma bazy"
-          
-          
-          
-          #echo $SQL >microserviceinit.sql
-          #mysql -h $MYSQLPODIP  -uroot -psecret  --port 3306  mysql -e "$SQL"
-          mysql -h $MYSQLPODIP  -uroot -psecret  --port 3306  mysql < /vagrant/sql/microserviceinit.sql
-        else
-          echo "jest baza"
-    	fi
-    
-    
-    
-          #------------------------------- mysql init ------------------------------- 
-    
-          #------------------------------- kafka init ------------------------------- 
-     
-      sudo apt install -y openjdk-8-jdk
-      while ! kubectl get po -o wide | grep kafka-0 | grep Running ; do   echo "waiting for kafka IP..." ; sleep 20 ; done
-
-
-      KAFKAPODIP=`kubectl get po -o wide | grep kafka-0 | grep Running `
-      echo $KAFKAPODIP
-      KAFKAPODIP=`echo $KAFKAPODIP  | cut -d " " -f 6`
-      echo $KAFKAPODIP
-
-      while ! nc -z $KAFKAPODIP 9092; do   echo "waiting kafka to launch ..." ; sleep 20 ; done
-
-
-		cd /tmp
-		curl http://ftp.ps.pl/pub/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz | tar xvz
-		/tmp/kafka_2.11-1.0.0/bin/kafka-topics.sh --list --zookeeper $KAFKAPODIP:2181
-
-
-          #------------------------------- kafka init ------------------------------- 
+setupMYSQL
+setupkafka
           
           #moje
       #curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/prometheus.yaml?$(date +%s)"  | kubectl apply -f -
@@ -240,8 +287,16 @@ echo Dashboard IP $DASHBOARDPODIP
       curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/tcp-services-configmap.yaml | kubectl apply -f -
       curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/udp-services-configmap.yaml | kubectl apply -f -
       curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/rbac.yaml | kubectl apply -f -
-      curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml | kubectl apply -f -
-      
+      #curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml | kubectl apply -f -
+
+      #set 3 replicas
+      curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml | sed -e 's/  replicas: 1/  replicas: 3/g' | kubectl apply -f -
+
+      curl "https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml?$(date +%s)"  | kubectl apply -f -
+      #curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/ingress-service-nodeport.yaml?$(date +%s)"  | kubectl apply -f -
+
+	  kubectl patch svc -n ingress-nginx ingress-nginx  --type=json -p='[{"op": "replace", "path": "/spec/ports/0/nodePort", "value": 30080}]'
+
       # heapster
       curl https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/grafana.yaml | kubectl apply -f -
       curl https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/influxdb.yaml | kubectl apply -f -
@@ -250,15 +305,11 @@ echo Dashboard IP $DASHBOARDPODIP
       # w nowszej wersji musia³em dodaæ bo by³y b³êdy: Failed to list *v1.Node: nodes is forbidden: User "system:serviceaccount:kube-system:heapster" cannot list nodes at the cluster scope
       curl https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml | kubectl apply -f -
       
-      
-
-      #set 3 replicas
-      curl https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/with-rbac.yaml | sed -e 's/  replicas: 1/  replicas: 3/g' | kubectl apply -f -
-
+   
       # moje poprawki : dashboard
       curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/dashboard-service-ingress.yaml | kubectl apply -f -
       curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/grafana.yaml?$(date +%s)"  | kubectl apply -f -
-      curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/ingress-service-nodeport.yaml | kubectl apply -f -
+      
       #curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/influxdb-ingress.yaml | kubectl apply -f -
       #curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/graphite.yaml | kubectl apply -f -
 
@@ -266,6 +317,6 @@ echo Dashboard IP $DASHBOARDPODIP
       echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioned "$1
            
       configureGrafana
-      showDashboardIP
       showCustomService
+      showDashboardIP
              
