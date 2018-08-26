3178440c97a7dcd0de65feb960d912d32a5a0868
marcin <marcin.kasinski@gmail.com>
2018-08-26 23:14:26 +0200

    modify es-exporter

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -320,6 +320,7 @@ curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/k
 
 #curl 'elasticsearch-logging:9200/_cat/indices?v'
 curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/elasticsearch.yaml?$(date +%s)" | kubectl apply -f -
+curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/elastic-exporter.yaml?$(date +%s)" | kubectl apply -f -
 
 curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/logstash.yaml?$(date +%s)" | kubectl apply -f -
 
