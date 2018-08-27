3fe711e032cd57be4896be7d4eedc6b6b542dce9
marcin <marcin.kasinski@gmail.com>
2018-08-28 00:22:24 +0200

    Add Doland with Poland

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -199,10 +199,13 @@ curl -XPOST --data "$DASHBOARD" -H "Content-Type:application/json"  http://admin
 DASHBOARD="{\"dashboard\":  $(</vagrant/conf/grafana_dashboard_mongo.json) }"
 curl -XPOST --data "$DASHBOARD" -H "Content-Type:application/json"  http://admin:admin@$GRAFANAPODIP:3000/api/dashboards/db
 
-
-
 DASHBOARD="{\"dashboard\":  $(</vagrant/conf/grafana_dashboard_mysql.json) }"
 curl -XPOST --data "$DASHBOARD" -H "Content-Type:application/json"  http://admin:admin@$GRAFANAPODIP:3000/api/dashboards/db
+
+DASHBOARD="{\"dashboard\":  $(</vagrant/conf/grafana_dashboard_poland.json) }"
+curl -XPOST --data "$DASHBOARD" -H "Content-Type:application/json"  http://admin:admin@$GRAFANAPODIP:3000/api/dashboards/db
+
+
 
 
 }
