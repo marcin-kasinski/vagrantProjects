e557913c48130d58a7b90088b2ce58df9dfb30ec
marcin <marcin.kasinski@gmail.com>
2018-10-06 07:43:12 +0200

    set static K8S_VERSION=1.12.0

--- kubernetes/scripts/init.sh
+++ kubernetes/scripts/init.sh
@@ -1,47 +1,47 @@
-
-
 start=$(date +%s)
 
 echo $start > ~/start_time
 
+sudo sh -c "echo 'export PATH=$PATH:/vagrant/scripts' >> /home/vagrant/.bashrc"
 
-      sudo sh -c "echo 'export PATH=$PATH:/vagrant/scripts' >> /home/vagrant/.bashrc"
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioning "$1
 
-      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioning "$1
+sudo sh -c "echo '192.168.1.11 k8smaster.local' >> /etc/hosts"
+sudo sh -c "echo '192.168.1.12 k8snode1.local' >> /etc/hosts"
+sudo sh -c "echo '192.168.1.13 k8snode2.local' >> /etc/hosts"
+sudo sh -c "echo '192.168.1.14 k8snode3.local' >> /etc/hosts"
+sudo sh -c "echo '192.168.1.12 springbootmicroserviceingress' >> /etc/hosts"
 
-      sudo sh -c "echo '192.168.1.11 k8smaster.local' >> /etc/hosts"
-      sudo sh -c "echo '192.168.1.12 k8snode1.local' >> /etc/hosts"
-      sudo sh -c "echo '192.168.1.13 k8snode2.local' >> /etc/hosts"
-      sudo sh -c "echo '192.168.1.14 k8snode3.local' >> /etc/hosts"
-      sudo sh -c "echo '192.168.1.12 springbootmicroserviceingress' >> /etc/hosts"
-      
-      sudo swapoff -a  
-      sudo sed -i -r '/swap/ s/^(.*)$/#\1/g' /etc/fstab
-      sudo sed -i -r '/cdrom/ s/^(.*)$/#\1/g' /etc/apt/sources.list
-      sudo apt -y update
-      sudo apt -y install -y docker.io
-      sudo apt install -y curl jq
-      sudo apt install -y apt-transport-https
-      sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-      sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"> ~/kubernetes.list 
-      sudo mv ~/kubernetes.list /etc/apt/sources.list.d/kubernetes.list
-      sudo apt update
-            
-      sudo service docker start
-            
-      # get kubernetes stable version      
-      export K8S_VERSION=$(curl -sSL https://dl.k8s.io/release/stable.txt)
-      
-      #remove 'v' character
-      K8S_VERSION=${K8S_VERSION//v}
-      echo $K8S_VERSION
-      
-#      sudo apt install -qy kubelet=1.11.3-00 kubeadm=1.11.3-00  kubectl=1.11.3-00   kubernetes-cni 
-      sudo apt install -y kubelet=${K8S_VERSION}-00 kubeadm=${K8S_VERSION}-00  kubectl=${K8S_VERSION}-00   kubernetes-cni 
+sudo swapoff -a  
+sudo sed -i -r '/swap/ s/^(.*)$/#\1/g' /etc/fstab
+sudo sed -i -r '/cdrom/ s/^(.*)$/#\1/g' /etc/apt/sources.list
+sudo apt -y update
+sudo apt -y install -y docker.io
+sudo apt install -y curl jq
+sudo apt install -y apt-transport-https
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo echo "deb http://apt.kubernetes.io/ kubernetes-xenial main"> ~/kubernetes.list 
+sudo mv ~/kubernetes.list /etc/apt/sources.list.d/kubernetes.list
+sudo apt update
 
-      #kubectl get nodes
-      
-      echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioned "$1
+sudo service docker start
 
-      # nfs biblioteki klienckie
-      sudo apt-get install -y nfs-common
+# get kubernetes stable version
+export K8S_VERSION=$(curl -sSL https://dl.k8s.io/release/stable.txt)
+
+#remove 'v' character
+K8S_VERSION=${K8S_VERSION//v}
+echo $K8S_VERSION
+
+#static
+K8S_VERSION=1.12.0
+
+#sudo apt install -qy kubelet=1.11.3-00 kubeadm=1.11.3-00  kubectl=1.11.3-00   kubernetes-cni 
+sudo apt install -y kubelet=${K8S_VERSION}-00 kubeadm=${K8S_VERSION}-00  kubectl=${K8S_VERSION}-00   kubernetes-cni 
+
+#kubectl get nodes
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioned "$1
+
+# nfs biblioteki klienckie
+sudo apt-get install -y nfs-common
