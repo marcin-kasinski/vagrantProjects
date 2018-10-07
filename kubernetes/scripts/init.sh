d3f8676b5382eeb6b4b5eb1978446cb67ff421ef
marcin <marcin.kasinski@gmail.com>
2018-10-07 18:24:37 +0200

    .

--- kubernetes/scripts/init.sh
+++ kubernetes/scripts/init.sh
@@ -34,10 +34,11 @@ K8S_VERSION=${K8S_VERSION//v}
 echo $K8S_VERSION
 
 #static
-K8S_VERSION=1.12.0
+#K8S_VERSION=1.12.0
 
 #sudo apt install -qy kubelet=1.11.3-00 kubeadm=1.11.3-00  kubectl=1.11.3-00   kubernetes-cni 
 #sudo apt install -y kubelet=${K8S_VERSION}-00 kubeadm=${K8S_VERSION}-00  kubectl=${K8S_VERSION}-00   kubernetes-cni 
+#sudo apt install -y kubelet kubeadm kubectl kubernetes-cni 
 sudo apt install -y kubelet kubeadm kubectl kubernetes-cni=0.6.0-00 
 
 #list releases
