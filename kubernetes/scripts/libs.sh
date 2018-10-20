a7e814880d364bbee2df9c48794defad832891fc
marcin <marcin.kasinski@gmail.com>
2018-10-20 08:02:50 +0200

    .

--- kubernetes/scripts/libs.sh
+++ kubernetes/scripts/libs.sh
@@ -242,7 +242,7 @@ sudo chown vagrant:vagrant /home/vagrant/.kube/config
 sudo cp -i /etc/kubernetes/admin.conf /var/nfs/kubernetes_share/
 
 #taint pods on master nodes
-kubectl taint nodes --all node-role.kubernetes.io/master-
+#kubectl taint nodes --all node-role.kubernetes.io/master-
 
 #kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
