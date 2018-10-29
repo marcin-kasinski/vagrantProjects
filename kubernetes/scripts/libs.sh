7a3f6a0093e6d3388882a1f949ff5167af9bc0d0
marcin <marcin.kasinski@gmail.com>
2018-10-29 13:35:25 +0100

    .

--- kubernetes/scripts/libs.sh
+++ kubernetes/scripts/libs.sh
@@ -211,7 +211,10 @@ IP=$( ifconfig enp0s8 | grep "inet addr:" | cut -d: -f2 | awk '{ print $1}' )
 
 #sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address $IP |  grep "kubeadm join"  >join_command
 #--------
-sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address $IP  >kubeadm_join
+#sudo kubeadm init --pod-network-cidr 10.244.0.0/16 --apiserver-advertise-address $IP  >kubeadm_join
+
+#for weave networking
+sudo kubeadm init --pod-network-cidr 10.32.0.0/12 --apiserver-advertise-address $IP  >kubeadm_join
 
 cat kubeadm_join
 cat kubeadm_join |  grep "kubeadm join"  >join_command
