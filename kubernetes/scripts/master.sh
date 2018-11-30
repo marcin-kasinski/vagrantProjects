7a3f419f5cd5054dcfbfa4704460c4cf69518579
marcin <marcin.kasinski@gmail.com>
2018-11-30 11:30:52 +0100

    add apt install -y default-jdk

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 source /vagrant/scripts/libs.sh
 
-sudo apt install -y openjdk-9-jdk
-
+#sudo apt install -y openjdk-9-jdk
+sudo apt install -y default-jdk
 
 echo I am provisioning...
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>machine provisioning "$1
