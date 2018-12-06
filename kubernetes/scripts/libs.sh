07acda5eb6715156fff9b48089c71f6072a6e3e0
marcin <marcin.kasinski@gmail.com>
2018-12-06 16:01:35 +0100

    add createfnproject

--- kubernetes/scripts/libs.sh
+++ kubernetes/scripts/libs.sh
@@ -1,6 +1,29 @@
 
 CLIPASS="secret"
 
+createfnproject()
+{
+
+
+##DEFAULT AS WARNING
+kubectl create clusterrolebinding fnproject --clusterrole=cluster-admin --serviceaccount kube-system:default
+
+git clone https://github.com/fnproject/fn-helm.git
+cd fn-helm
+#Install chart dependencies (from requirements.yaml):
+helm dep build fn
+
+#Then install the chart. I chose the release name fm-release:
+helm install --name fm-release fn
+#patch ui service
+
+kubectl patch svc fm-release-fn-ui --type=json -p='[{"op": "replace", "path": "/spec/type", "value": "NodePort"}]'
+
+curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/fnproject.yaml?$(date +%s)"  | kubectl apply -f -
+
+
+}
+
 remove_LVM_logical_volume(){
 
 #list partitions
