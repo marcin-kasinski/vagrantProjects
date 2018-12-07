e40437c19c5e24f8bc8ab7cd55e602f3d35b93a5
marcin <marcin.kasinski@gmail.com>
2018-12-07 10:28:35 +0100

    add fn build

--- kubernetes/scripts/createfnproject.sh
+++ kubernetes/scripts/createfnproject.sh
@@ -18,6 +18,30 @@ kubectl patch svc fm-release-fn-api --type=json -p='[{"op": "replace", "path": "
 
 curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/fnproject.yaml?$(date +%s)"  | kubectl apply -f -
 
+kubectl get svc --namespace default fm-release-fn-api
+#export FN_API_URL=http://$(kubectl get svc --namespace default fm-release-fn-api -o jsonpath='{.status.loadBalancer.ingress[0].ip}'):80
+export FN_API_URL=http://$(kubectl get svc --namespace default fm-release-fn-api -o jsonpath='{.spec.clusterIP}'):80
+echo $FN_API_URL
+
+
+# install fn on local nachine
+curl -LSs https://raw.githubusercontent.com/fnproject/cli/master/install | sh
+
+
+fn version
+
+fn list contexts
+
+#https://fnproject.io/tutorials/JavaFDKIntroduction/
+#firstfunction
+fn init --runtime java --trigger http javafnmktst
+cd javafnmktst
+cat func.yaml
+export FN_REGISTRY=marcinkasinski
+#docker login
+fn --verbose build
+#fn --verbose deploy --registry marcinkasinski --app java-app
+#fn list triggers java-app
 }
 
 
