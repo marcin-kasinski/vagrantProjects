5e30ff284ef506c31c1fb89da2dd47150e64908d
marcin <marcin.kasinski@gmail.com>
2018-12-13 10:59:41 +0100

    change installHelm order

--- kubernetes/scripts/master.sh
+++ kubernetes/scripts/master.sh
@@ -13,11 +13,10 @@ setupJava 2>&1 | tee ~/setupJava.log
 
 #configure_nfs 2>&1 | tee ~/configure_nfs.log
 init_kubernetes 2>&1 | tee ~/init_kubernetes.log
+installHelm 2>&1 | tee ~/installHelm.log
 
 setupIstio 2>&1 | tee ~/setupIstio.log
 createKiali 2>&1 | tee ~/createKiali.log
-
-installHelm 2>&1 | tee ~/installHelm.log
 
 setupSSL 2>&1 | tee ~/setupSSL.log
 #install_cfssl
