fee92f083eaa9916f5597f6d4f3c6270fd4c8692
marcin <marcin.kasinski@gmail.com>
2018-12-19 16:10:14 +0100

    .

--- kubernetes/scripts/libs.sh
+++ kubernetes/scripts/libs.sh
@@ -132,6 +132,7 @@ cd $ISTIO_VERSION
 
 #helm install install/kubernetes/helm/istio --name istio --namespace istio-system  -f install/kubernetes/helm/istio/values-istio-galley.yaml
 #helm install install/kubernetes/helm/istio --name istio --namespace istio-system --set global.proxy.includeIPRanges="10.32.0.0/12"
+#helm install install/kubernetes/helm/istio --name istio --namespace istio-system --set gateways.istio-ingressgateway.type=ClusterIP
 #kubectl apply -f install/kubernetes/istio-demo-auth.yaml
 kubectl apply -f install/kubernetes/istio-demo.yaml
 
