b7bcd9352bb9b054f47cbf79329ac7e1085d341e
marcin <marcin.kasinski@gmail.com>
2019-01-04 16:53:24 +0100

    delete istio-galley

--- kubernetes/scripts/libs.sh
+++ kubernetes/scripts/libs.sh
@@ -278,7 +278,7 @@ kubectl delete svc -n istio-system prometheus
 kubectl delete deployment -n istio-system grafana
 kubectl delete deployment -n istio-system prometheus
 # poniższe usuwam, bo z nim nie mogę zdefiniować VirtualService zawierające host z portem
-kubectl delete deployment -n istio-system galley
+kubectl delete deployment -n istio-system istio-galley
 
 }
 
