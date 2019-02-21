008d36f1a082f203a4072edbe326d6129725c0ff
marcin <marcin.kasinski@gmail.com>
2019-02-21 09:00:45 +0100

    correct test

--- kubernetes/scripts/test.sh
+++ kubernetes/scripts/test.sh
@@ -3,8 +3,8 @@ source /vagrant/scripts/libs.sh
 
 createJaegerOperator 2>&1 | tee ~/createJaegerOperator.log
 
-setupIstio 2>&1 | tee ~/setupIstio.log
-createVistio 2>&1 | tee ~/createVistio.log
+#setupIstio 2>&1 | tee ~/setupIstio.log
+#createVistio 2>&1 | tee ~/createVistio.log
 createKiali 2>&1 | tee ~/createKiali.log
 
 setupSSL apps 2>&1 | tee ~/setupSSL.log
@@ -16,12 +16,14 @@ setupkafka 2>&1 | tee ~/setupkafka.log
 
 createIngress 2>&1 | tee ~/createIngress.log
 
-createHeapster 2>&1 | tee ~/createHeapster.log
+#createHeapster 2>&1 | tee ~/createHeapster.log
 
-createMyBackendServers 2>&1 | tee ~/createMyBackendServers.log
+#createMyBackendServers 2>&1 | tee ~/createMyBackendServers.log
+curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/rabbitmq.yaml | kubectl apply -f -
 
-createMyapps 2>&1 | tee ~/createMyapps.log
-   
+#createMyapps 2>&1 | tee ~/createMyapps.log
+curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/springbootweb.yaml?$(date +%s)"  | kubectl apply -n apps -f -
+  
 # moje poprawki : dashboard
 curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/dashboard-service-ingress.yaml | kubectl apply -f -
 
