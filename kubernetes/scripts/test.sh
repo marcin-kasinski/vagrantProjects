70dd20bcd7aecf69e3796f228b2dfaa271e09550
marcin <marcin.kasinski@gmail.com>
2019-04-20 19:51:12 +0200

    .

--- kubernetes/scripts/test.sh
+++ kubernetes/scripts/test.sh
@@ -4,21 +4,19 @@ source /vagrant/scripts/libs.sh
 #setupIstio 2>&1 | tee ~/setupIstio.log
 setupIstio1_0_7 2>&1 | tee ~/setupIstio1_0_7.log
 setupJava 2>&1 | tee ~/setupJava.log
-setupSSL apps 2>&1 | tee ~/setupSSL.log
+#setupSSL apps 2>&1 | tee ~/setupSSL.log
 
 createIngress 2>&1 | tee ~/createIngress.log
 #curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/nginx.yaml?$(date +%s)"  | kubectl apply -f -
 #curl https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/rabbitmq.yaml | kubectl apply -f -
 #curl "https://raw.githubusercontent.com/marcin-kasinski/vagrantProjects/master/kubernetes/yml/springbootweb.yaml?$(date +%s)"  | kubectl apply -n apps -f -
 
-
 #kubectl apply -f /vagrant/yml/httpbin.yaml
 
-kubectl apply -n apps -f /vagrant/yml/springbootsoapservice.yaml
-
+#kubectl apply -n apps -f /vagrant/yml/springbootsoapservice.yaml
+kubectl apply -n apps -f /vagrant/yml/springbootwebreactor.yaml
 finish
 #exit
-
 
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.clusterIP}')
 export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
@@ -41,14 +39,13 @@ log "checking internal"
 #curl -I -H Host:springbootweb.apps.svc.cluster.local http://$INGRESS_HOST:$INGRESS_PORT/
 # curl -H Host:springbootsoapservice.apps.svc.cluster.local http://10.44.0.10:8080/ws/countries.wsdl | grep operation
 curl -H "Host: springbootwebreactor.apps.svc.cluster.local" http://$INGRESS_HOST:$INGRESS_PORT
-curl -H "Host: springbootwebreactor.apps.svc.cluster.local" localhost:30999
+#curl -H "Host: springbootwebreactor.apps.svc.cluster.local" localhost:30999
 
 log "checking external"
 curl -H "Host: springbootwebreactor.apps.svc.cluster.local" localhost:30999
 exit
 
 createJaegerOperator 2>&1 | tee ~/createJaegerOperator.log
-
 
 #createVistio 2>&1 | tee ~/createVistio.log
 createKiali 2>&1 | tee ~/createKiali.log
