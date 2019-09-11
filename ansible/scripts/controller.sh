70ccdf9c1dc32ccc7321f41d568674862ea2d6dd
marcin <marcin.kasinski@gmail.com>
2019-09-11 12:25:12 +0200

    add comments

--- ansible/scripts/controller.sh
+++ ansible/scripts/controller.sh
@@ -13,20 +13,32 @@ sudo apt install -y ansible
 
 #ansible -m ping all -i /vagrant/playbooks/inventory
 
+# -b run as root on target host
+#ansible -m ping all -i /vagrant/playbooks/inventory -b 
+
 #get fingerprint
-ssh-keygen -lf ~/.ssh/id_rsa.pub
+#ssh-keygen -lf ~/.ssh/id_rsa.pub
 #get fingerprint MD5
-ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub
+#ssh-keygen -E md5 -lf ~/.ssh/id_rsa.pub
 
 
 #listowanie
-ssh-agent bash
-ssh-add ~/.ssh/id_rsa
-ssh-add -l
-ssh-add -l -E MD5
-ssh-add -l -E ECDSA
+#ssh-agent bash
+#ssh-add ~/.ssh/id_rsa
+#ssh-add -l
+#ssh-add -l -E MD5
+#ssh-add -l -E ECDSA
 
 
 
 #ssh -i ./.ssh/id_rsa vagrant@192.168.1.12
 #ssh vagrant@192.168.1.12
+
+#list fingerprints
+cd ~/.ssh
+find . -type f -exec printf "\n{}\n" \; -exec ssh-keygen -E md5 -lf {} \;
+
+
+
+
+echo Controller finished...
